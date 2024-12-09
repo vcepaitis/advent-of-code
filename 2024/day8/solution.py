8843c5f04f5614f00bef079280e56b9febc7fb6e
Vilius Cepaitis <vilius.cepaitis@cern.ch>
2024-12-09 20:03:49 +0100

    first part

---
+++ 2024/day8/solution.py
@@ -0,0 +1,59 @@
+import numpy as np
+from collections import defaultdict
+from itertools import combinations
+data = []
+
+with open("input") as f:
+    for l in f:
+        row = list(l.strip())
+        data.append(row)
+
+data = np.asarray(data)
+print(data)
+
+# relevant frequencies
+values, counts = np.unique_counts(data)
+
+freq_dict = defaultdict(list)
+
+
+def is_diagonal(first, second):
+    dx = abs(first[0] - second[0])
+    dy = abs(first[1] - second[1])
+    return (dx == 0) or (dy == 0) or (dx == dy)
+    
+
+def get_dist(first, second):
+    return np.square(first[0] - second[0]) + np.square(first[1] - second[1])
+
+# loop over all positions to find frequencies
+for x in range(data.shape[0]):
+    for y in range(data.shape[1]):
+        freq = data[x, y]
+        if freq != ".":
+            freq_dict[freq].append((x, y))
+
+freq_dict = {freq: positions for freq, positions in freq_dict.items() if len(positions) > 1}
+output_data = data
+antinodes = defaultdict(None)
+antinodes_2 = defaultdict(None)
+for x in range(data.shape[0]):
+    for y in range(data.shape[1]):
+        pos = (x, y)
+        # get distances 
+        for freq, test_positions in freq_dict.items():
+            # Get all combinations of two antennas
+            # (7, 6) -4 -2 -2 -1
+            for pos1, pos2 in combinations(test_positions, 2):
+                dx1 = pos1[0] - x
+                dy1 = pos1[1] - y
+                dx2 = pos2[0] - x
+                dy2 = pos2[1] - y
+                if (dx1 * 2 == dx2) and (dy1 * 2 == dy2) \
+                or ((dx2 * 2 == dx1) and (dy2 * 2 == dy1)):
+                    antinodes[pos] = True
+                    output_data[pos] = "#"
+
+# print(output_data)
+print(antinodes)
+print(len(antinodes))
