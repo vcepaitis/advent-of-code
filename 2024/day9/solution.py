f7d5a2c2e7a19be4bd5e0203a754472a7e2143dc
Vilius Cepaitis <vilius.cepaitis@cern.ch>
2024-12-10 08:13:51 +0100

    up

--- 2024/day9/solution.py
+++ 2024/day9/solution.py
@@ -4,16 +4,18 @@ from numba import njit
 @njit
 def sort_disk(disk):
     ndisk = len(disk)
+    j_current = ndisk-1
     for i in range(ndisk):
         if disk[i] == ".":
-            for j in range(ndisk-1, i, -1):
+            for j in range(j_current, i, -1):
                 if disk[j] != ".":
                     disk[i] = disk[j]
                     disk[j] = "."
+                    j_current = j
                     break
     return disk
 diskmap = str(12345)
-# diskmap = str(2333133121414131402)
+diskmap = str(2333133121414131402)
 diskmap = open("input").readline().strip()
 disk = []
 
@@ -28,8 +30,11 @@ for input in diskmap:
 
     free = not free
 
-
+print(disk)
+disk = np.asarray(disk)
 disk = sort_disk(disk)
+with open("output", "w") as f:
+    print("".join(disk), file=f)
 checksum = 0
 
 for i, x in enumerate(disk):
