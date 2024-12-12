f7cdc901fe536f92497ec1d5bc1906a7f5361553
Vilius Cepaitis <vilius.cepaitis@cern.ch>
2024-12-12 16:26:02 +0100

    fast sol

--- 2024/day11/solution_fast.py
+++ 2024/day11/solution_fast.py
@@ -1,44 +1,36 @@
-import sys
-sys.setrecursionlimit(100000)
-
-rules = {0: [1]}
-
-def split_stone(stone, stones, n=0, nmax=5):
-    # Can no longer split
-    if n == nmax:
-        return stones + [stone]
-
-    if stone in rules:
-        for new_stone in rules[stone]:
-            stones = split_stone(new_stone, stones, n+1, nmax)
-
-    elif stone == 0:
-        stones = split_stone(1, stones, n+1, nmax)
-
-    elif len(str(stone)) % 2 == 0:
-        stone = str(stone)
-        nsplit = len(stone) // 2
-        new_stone_1 = int(str(stone[0:nsplit]))
-        new_stone_2 = int(str(stone[nsplit:]))
-        rules[int(stone)] = [new_stone_1, new_stone_2]
-        stones = split_stone(new_stone_1, stones, n+1, nmax)
-        stones = split_stone(new_stone_2, stones, n+1, nmax)
-    else:
-        rules[stone] = [stone*2024]
-        stones = split_stone(stone*2024, stones, n+1, nmax)
-
-    return stones
-
+from collections import Counter
 
 with open("input") as f:
     stones = f.readline().strip().split()
 
+print(stones)
 stones = [int(stone) for stone in stones]
-
-new_stones = []
-
+stones_dict = Counter()
 for stone in stones:
-    split_stones = split_stone(stone, [], n=0, nmax=25)
-    new_stones.extend(split_stones)
+    stones_dict[stone] += 1
 
-print(len(new_stones))
+print(stones_dict)
+
+for i in range(75):
+    print(f"Iteration {i}")
+    new_stones_dict = Counter()
+    for stone, count in stones_dict.items():
+        if stone == 0:
+            new_stone = 1
+            new_stones_dict[new_stone] += count
+        elif len(str(stone)) % 2 == 0:
+            stone = str(stone)
+            nsplit = len(stone) // 2
+            new_stone_1 = int(str(stone[0:nsplit]))
+            new_stone_2 = int(str(stone[nsplit:]))
+            new_stones_dict[new_stone_1] += count
+            new_stones_dict[new_stone_2] += count
+        else:
+            new_stones_dict[stone*2024] += count
+    stones_dict = new_stones_dict
+
+total = 0
+for stone, count in stones_dict.items():
+    total += count
+
+print(total)
