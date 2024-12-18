6bfc4b8c443be7aba778df861f833ecbc9708123
Vilius Cepaitis <vilius.cepaitis@cern.ch>
2024-12-18 22:16:27 +0100

    maximos dezes

--- 2024/day15/solution.py
+++ 2024/day15/solution.py
@@ -1,9 +1,11 @@
 import sys
 sys.path.append("../..")
 from utils import read_grid_and_rest
+from collections import deque
+from tqdm import tqdm
 from copy import copy
 import numpy as np
-np.set_printoptions(threshold=sys.maxsize, linewidth=300)
+np.set_printoptions(threshold=sys.maxsize, linewidth=3000)
 
 if len(sys.argv) == 2:
     f = sys.argv[1]
@@ -38,64 +40,72 @@ pos = (int(start[0][0]), int(start[1][0]))
 print("Initial state:")
 print(grid)
 print()
-for instruction in instructions:
+for instruction in tqdm(instructions):
+    # @.
     if instruction == "<":
         test = [pos[0], pos[1]-1]
         test2 = [pos[0], pos[1]-2]
-        test3 = [pos[0], pos[1]-3]
     elif instruction == ">":
         test = [pos[0], pos[1]+1]
         test2 = [pos[0], pos[1]+2]
-        test3 = [pos[0], pos[1]+3]
     elif instruction == "^":
         test = [pos[0]-1, pos[1]]
         test2 = [pos[0]-2, pos[1]]
-        test3 = [pos[0]-3, pos[1]]
     elif instruction == "v":
         test = [pos[0]+1, pos[1]]
         test2 = [pos[0]+2, pos[1]]
-        test3 = [pos[0]+3, pos[1]]
     else:
         raise ValueError()
 
+    # Easy case: just move
     if grid[*test] == ".":
         grid[*pos] = "."
         grid[*test] = "@"
         pos = test
+    # Difficult case: move potentially several crates at once 
     elif grid[*test] == "[" or grid[*test] == "]":
+        # The direction in which we're moving
         test_dir = [test2[0] - test[0], test2[1] - test[1]]
-        move = True
+        boxes = deque([test])
+
+        if grid[*test] == "[":
+            box_right = [test[0], test[1]+1]
+            boxes.append(box_right)
+        elif grid[*test] == "]":
+            box_left = [test[0], test[1]-1]
+            boxes.append(box_left)
+            
+        # determine all boxes that are in the way
+        box_coords = []
+        while boxes:
+            element = boxes.popleft()
+            if element not in box_coords:
+                box_coords.append(element)
+
+            test_new = [element[0] + test_dir[0], element[1] + test_dir[1]]
+            if grid[*test_new] == "[" or grid[*test_new] == "]":
+                boxes.append(test_new)
+                if test_dir == [-1, 0] or test_dir == [1, 0]:
+                    if grid[*test_new] == "[":
+                        box_right = [test_new[0], test_new[1]+1]
+                        boxes.append(box_right)
+                    elif grid[*test_new] == "]":
+                        box_left = [test_new[0], test_new[1]-1]
+                        boxes.append(box_left)
+
+        # Now let's see if we can actually move all the boxes
         new_grid = copy(grid)
-        new_grid[*test] = "@"
-        new_grid[*pos] = "."
-
-        if test3[0] < 0 or test3[0] == nx:
-            move = False
-        if test3[1] < 0 or test3[1] == ny:
-            move = False
-
-        while move:
-
-            if new_grid[*test3] == "#":
+        move = True
+        for box in reversed(box_coords):
+            new_space = [box[0] + test_dir[0], box[1] + test_dir[1]]
+            if new_grid[*new_space] == ".":
+                new_grid[*new_space], new_grid[*box] = new_grid[*box], new_grid[*new_space]
+            else:
                 move = False
-            elif new_grid[*test3] == "[" or new_grid[*test3] == "]":
-                test2[0] += test_dir[0]
-                test2[1] += test_dir[1]
-                test3[0] += test_dir[0]
-                test3[1] += test_dir[1]
-
-            elif new_grid[*test3] == ".":
-
-                if new_grid[*test2] == "[":
-                    new_grid[*test2] = "]"
-                    new_grid[*test3] = "["
-
-                elif new_grid[*test2] == "]":
-                    new_grid[*test2] = "["
-                    new_grid[*test3] = "]"
-
                 break
         if move:
+            new_grid[*test] = "@"
+            new_grid[*pos] = "."
             grid = new_grid
             pos = test
 
@@ -121,9 +131,9 @@ for instruction in instructions:
     #         pos = test
 
 
-    print(f"Move {instruction}:")
-    print(grid)
-    print()
+    # print(f"Move {instruction}:")
+    # print(grid)
+    # print()
 
 total_coords = 0
 for ix, iy in np.ndindex(grid.shape):
